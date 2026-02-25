8a4807bab902811d108ac617cc4303e3baa19168
SayemImran <ik262142@gmail.com>
2026-02-25 23:42:17 +0600

    backemd order issue

--- order/serializers.py
+++ order/serializers.py
@@ -14,29 +14,6 @@ class SimpleProductSerializer(serializers.ModelSerializer):
         fields = ['id','name','price']
 
 
-# class AddCartItemSerializer(serializers.ModelSerializer):
-#     product_id = serializers.IntegerField()
-#     class Meta:
-#         model = CartItem
-#         fields =['id','product_id','quantity']
-
-#     def save(self,**kwargs):
-#         cart_id =self.context['cart_id']
-#         product_id = self.validated_data['product_id']
-#         quantity = self.validated_data['quantity']
-
-#         try:
-#             cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
-#             cart_item.quantity+= quantity
-#             self.instance = cart_item.save()
-#         except CartItem.DoesNotExist:
-#             self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
-#         return self.instance
-    
-#     def validate_product_id(self, value):
-#         if not Product.objects.filter(pk=value).exists():
-#             raise serializers.ValidationError(f"Product with {value} does not exits")
-#         return value
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
 
@@ -111,6 +88,7 @@ class CreateOrderSerializer(serializers.Serializer):
             order = OrderService.create_order(user_id=user_id, cart_id=cart_id)
         except ValueError as e:
             raise serializers.ValidationError(str(e))
+        return order
         
     def to_representation(self, instance):
         return OrderSerializer(instance).data
@@ -125,17 +103,6 @@ class UpdateOrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['status']
 
-    # def update(self, instance, validated_data):
-    #     user = self.context['user']
-    #     new_status = validated_data['status']
-
-    #     if new_status == Order.CANCELED:
-    #         return OrderService.cancel_order(order=instance,user=user)
-    #     if not user.is_staff:
-    #         raise serializers.ValidationError({"detail":"You are not allowed to change the order status"})
-        
-    #     return super().update(instance,validated_data)
-    
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
     class Meta:
